6ebd6cc0435f41aecb80a26d8bb67d15d13c45ea
unknown <freelancestanislav@gmail.com>
2023-01-11 18:20:07 +0300

    start

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express()
+const PORT = process.env.PORT || 5000
+const delay = 60000
+
+function start() {
+    setInterval(() => {
+        axios.get("https://vsekinopoisk.onrender.com/")
+    }, delay)    
+}
+
+app.listen(PORT, () => {
+    console.log(`Server start no port ${PORT}`);
+    start()
+})
